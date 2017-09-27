6dc13009796ad7024502796d1c0807db40af48fc
momirror <471488376@qq.com>
2017-09-27 10:34:51 +0800

    add secrets post interface

--- database/SecretDB.js
+++ database/SecretDB.js
@@ -44,7 +44,7 @@ let deleteSecret = async (id) => {
 
 //查询最新的count个数据
 let findLatestSecretsWithCount = async (count)=> {
-    let sql = `select * from comments order by id desc limit ?`;
+    let sql = `select * from secrets order by id desc limit ?`;
     let results = await db.query(sql,count);
 
     //因为上面查出来的数据是逆序的，这里再逆一下，转回正序
--- routes/secrets.js
+++ routes/secrets.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const Secret = require('../model/Secret');
 const SecretDB = require('../database/SecretDB');
+const TimeUtil = require('../Utils/time');
 
 router.prefix('/secrets')
 
@@ -15,6 +16,28 @@ router.get('/bar', function (ctx, next) {
 })
 
 router.get('/test2', function (ctx, next) {
+
     ctx.body = 'this is a secrets/test response'
 })
+
+
+/*****************************POST************************************/
+router.post('/', async function (ctx, next) {
+    console.log('post........');
+    console.log(ctx.request.body);
+    let info = ctx.request.body;
+    let content = info.content;
+
+    let ownerId = -1;
+    if(info.ownerId != undefined){
+        ownerId = info.ownerId;
+    }
+    var secret = new Secret(ownerId,content,TimeUtil.getCurrentTimestamp());
+    SecretDB.addSecret(secret);
+
+    ctx.body = 'post successful!!'
+
+})
+
+
 module.exports = router
